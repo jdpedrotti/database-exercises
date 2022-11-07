b0b6f5a7034b748c8845b534b6c2c01d9a88448c
John Pedrotti <jdpedrotti@gmail.com>
2022-11-07 15:39:17 -0600

    complete sql select ex with captions and few corrections

--- basic_statements.sql
+++ basic_statements.sql
@@ -5,3 +5,4 @@ VALUES ('Tupac','2Pacalypse Now!', 1991, 0.923, 'hiphop');
 
 INSERT INTO albums (artist_name, album_name, release_date, sales, genre)
 VALUES ('Backstreet Boys','Millennium', 1999, 24, 'pop');
+
--- select_exercises.sql
+++ select_exercises.sql
@@ -1,13 +1,15 @@
 USE codeup_test_db;
 
-SELECT album_name FROM albums WHERE artist_name = 'Pink Floyd';
+SELECT * FROM albums;
+
+SELECT * FROM albums WHERE artist_name = 'Pink Floyd';
 
 SELECT release_date FROM albums WHERE album_name = 'Sgt. Peppers';
 
 SELECT genre FROM albums WHERE album_name = 'Back in Black';
 
-SELECT album_name FROM albums WHERE release_date > 1989;
+SELECT album_name, release_date FROM albums WHERE release_date BETWEEN 1990 AND 2000 ORDER BY release_date;
 
-SELECT album_name FROM albums WHERE sales < 20;
+SELECT 'Less than 20 million: ' AS caption, album_name, sales FROM albums WHERE sales < 20 ORDER BY sales DESC;
 
 SELECT album_name FROM albums WHERE genre = 'Rock';

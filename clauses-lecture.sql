b5dea9a5aaa4bf22605f4232670e07bf7401c1f9
John Pedrotti <jdpedrotti@gmail.com>
2022-11-09 09:37:37 -0600

    complete sql functions ex

--- clauses-lecture.sql
+++ clauses-lecture.sql
@@ -26,3 +26,8 @@ SELECT birth_date, hire_date FROM employees
 
 SELECT * FROM employees WHERE birth_date LIKE '1954%-01' ORDER BY birth_date, last_name LIMIT 50 OFFSET 50;
 
+
+# underscore wildcard
+# however many underscores means however many character spaces before the letter e below
+SELECT last_name FROM employees
+         WHERE last_name LIKE '__e%' ORDER BY last_name;
---
+++ functions_exercises.sql
@@ -0,0 +1,47 @@
+USE employees;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
+
+# WORK AROUND FOR INTELLIJ PAGINATION DECEPTION
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, emp_no DESC;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name, emp_no DESC;
+
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name, first_name, emp_no DESC;
+
+
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
+
+SELECT * FROM employees WHERE last_name LIKE '%q%' ORDER BY emp_no DESC;
+
+SELECT * FROM employees
+WHERE (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya')
+  AND gender = 'M';
+
+
+
+SELECT * FROM employees
+WHERE (last_name LIKE 'E%' AND last_name LIKE '%e');
+
+SELECT * FROM employees
+WHERE (last_name LIKE '%q%' AND last_name NOT LIKE '%qu%');
+
+
+
+
+SELECT CONCAT (first_name, ' ', last_name) AS name FROM employees
+WHERE (last_name LIKE 'E%' OR last_name LIKE '%e');
+
+
+SELECT * FROM employees
+            WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
+            AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25
+            ORDER BY hire_date DESC;
+
+SELECT DATEDIFF(NOW(),hire_date) FROM employees
+                WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
+                AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25
+                ORDER BY hire_date DESC;
+
+
+

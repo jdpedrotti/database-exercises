323fa6cd1672072a6219935a825c62b92585f527
John Pedrotti <jdpedrotti@gmail.com>
2022-11-09 10:32:43 -0600

    complete sql functions ex with notes and some restructuring

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,5 +1,6 @@
 USE employees;
 
+# COPY OF ORDER BY EXERCISE
 SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name;
 
 # WORK AROUND FOR INTELLIJ PAGINATION DECEPTION
@@ -26,22 +27,20 @@ WHERE (last_name LIKE 'E%' AND last_name LIKE '%e');
 SELECT * FROM employees
 WHERE (last_name LIKE '%q%' AND last_name NOT LIKE '%qu%');
 
+##################################
 
-
-
+# FUNCTIONS EXERCISES
 SELECT CONCAT (first_name, ' ', last_name) AS name FROM employees
-WHERE (last_name LIKE 'E%' OR last_name LIKE '%e');
+WHERE (last_name LIKE 'E%' AND last_name LIKE '%e');
 
 
 SELECT * FROM employees
             WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
             AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25
-            ORDER BY hire_date DESC;
+            ORDER BY birth_date, hire_date DESC;
 
-SELECT DATEDIFF(NOW(),hire_date) FROM employees
+SELECT *,DATEDIFF(NOW(),hire_date) FROM employees
                 WHERE YEAR(hire_date) BETWEEN 1990 AND 1999
                 AND MONTH(birth_date) = 12 AND DAY(birth_date) = 25
-                ORDER BY hire_date DESC;
-
-
+                ORDER BY hire_date, birth_date DESC;
 
